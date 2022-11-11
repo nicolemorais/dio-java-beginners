2d0f26c9cc3d397245a85f7aa6bae9b49c291b0f
nicolemorais <nicolemoraisatt@gmail.com>
2022-11-11 00:32:11 -0300

    Métodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: " + smartTv.canal);
+    
+
+    }
+}
